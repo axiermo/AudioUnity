6f42ee93d40c043048e2816f545a80d4ff0b007f
Marc de Pedro <marcdp2012@gmail.com>
2019-06-10 20:34:09 +0200

    1.1.1, 1.1.2, 1.1.3

---
+++ Assets/MusicBehaviour.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicBehaviour : MonoBehaviour {
+
+    public AudioClip otherClip;
+    AudioSource audioSource;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+        if (!audioSource.isPlaying)
+        {
+            audioSource.clip = otherClip;
+            audioSource.Play();
+            audioSource.loop = true;
+        }
+    }
+}
